202043cffa69b7e3893e891af3d5ee7f28cee6e0
Azman Amin <azmanamin14@gmail.com>
2022-08-16 23:34:57 +0600

    take from config in integration test

--- tests/integration/seed/seed.js
+++ tests/integration/seed/seed.js
@@ -1,13 +1,14 @@
 const { copyFileSync } = require("fs");
 const DB = require("../../../src/fileserver/data/db.json");
 const { saveToDatabase } = require("../../../src/fileserver/data/utils");
+const config = require('../../../src/storageprovider/configs/config')
 
 const fileData = {
     originalname: "Node.js Backend code test.pdf",
     mimetype: "application/pdf",
-    destination: "/media/azman/Utilities/Projects/NodeJsProject/file-sharing-api-server/uploads",
+    destination: `${config.FOLDER}`,
     filename: "file-1660589701059.pdf",
-    path: "/media/azman/Utilities/Projects/NodeJsProject/file-sharing-api-server/uploads/file-1660589701059.pdf",
+    path: `${config.FOLDER}/file-1660589701059.pdf`,
     privatekey: "cd8f23c9-74c5-4df2-bd67-93154073c975",
     publickey: "7b7fc110-9b41-4016-adac-326c69933391",
     createdat: Date.now(),
